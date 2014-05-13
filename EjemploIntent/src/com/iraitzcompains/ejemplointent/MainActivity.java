1d3f86424a7dff40a9cd83dd78ac765e3999ebbb
Iraitz Compains <iracom82@gmail.com>
2014-05-13 12:43:09 +0200

    Se ha modificado el código para que se muestre una foto de alta calidad.

--- EjemploIntent/src/com/iraitzcompains/ejemplointent/MainActivity.java
+++ EjemploIntent/src/com/iraitzcompains/ejemplointent/MainActivity.java
@@ -1,11 +1,19 @@
 package com.iraitzcompains.ejemplointent;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,12 +26,15 @@ public class MainActivity extends Activity {
 	private static final int FORMULARIO = 1;
 	private static final int CAMARA = 2;
 	
+	private String mCurrentPhotoPath;
+	
 	EditText editText;
 	TextView textView;
 	ImageView imageView;
 	Button btnForm;
 	Button btnCamera;
 	Button btnContact;
+	File image;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +63,40 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-				startActivityForResult(intent, CAMARA);
+				
+				// Ensure that there's a camera activity to handle the intent
+			    if (intent.resolveActivity(getPackageManager()) != null) {
+			        // Create the File where the photo should go
+			        File photoFile = null;
+			        try {
+			            photoFile = createImageFile();
+			        } catch (IOException ex) {
+			            // Error occurred while creating the File
+			        	Log.d("INTENT","Error: " + ex.getMessage());
+			        }
+			        // Continue only if the File was successfully created
+			        if (photoFile != null) {
+			            intent.putExtra(MediaStore.EXTRA_OUTPUT,
+			                    Uri.fromFile(photoFile));
+			            startActivityForResult(intent, CAMARA);
+			        }
+			    }
 			}
 		});
+		
+	}
+	
+	private File createImageFile() throws IOException {
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    File storageDir = Environment.getExternalStorageDirectory();
+	    image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+	    return image;
 	}
 	
 	@Override
@@ -70,13 +112,38 @@ public class MainActivity extends Activity {
 			break;
 		case CAMARA:
 			if(resultCode == Activity.RESULT_OK) {
-				Bundle extras = data.getExtras();
+				/*Bundle extras = data.getExtras();
 		        Bitmap imageBitmap = (Bitmap) extras.get("data");
-				imageView.setImageBitmap(imageBitmap);
+				imageView.setImageBitmap(imageBitmap);*/
+				setPic();
 			}
 		default:
 			break;
 		}
+	}
+	
+	private void setPic() {
+	    // Get the dimensions of the View
+	    int targetW = imageView.getWidth();
+	    int targetH = imageView.getHeight();
+
+	    // Get the dimensions of the bitmap
+	    BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+	    bmOptions.inJustDecodeBounds = true;
+	    BitmapFactory.decodeFile(image.getPath(), bmOptions);
+	    int photoW = bmOptions.outWidth;
+	    int photoH = bmOptions.outHeight;
+
+	    // Determine how much to scale down the image
+	    int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+	    // Decode the image file into a Bitmap sized to fill the View
+	    bmOptions.inJustDecodeBounds = false;
+	    bmOptions.inSampleSize = scaleFactor;
+	    bmOptions.inPurgeable = true;
+
+	    Bitmap bitmap = BitmapFactory.decodeFile(image.getPath(), bmOptions);
+	    imageView.setImageBitmap(bitmap);
 	}
 	
 	
