9e744f8bfaa18e7b915d5b855e837213b78bd9ec
Iraitz Compains <iracom82@gmail.com>
2014-05-13 10:34:00 +0200

    Se ha añadido la opción de sacar una foto y mostrarla.

--- EjemploIntent/src/com/iraitzcompains/ejemplointent/MainActivity.java
+++ EjemploIntent/src/com/iraitzcompains/ejemplointent/MainActivity.java
@@ -2,29 +2,40 @@ package com.iraitzcompains.ejemplointent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	private static final int FORMULARIO = 1;
+	private static final int CAMARA = 2;
 	
-	Button btnForm;
 	EditText editText;
 	TextView textView;
+	ImageView imageView;
+	Button btnForm;
+	Button btnCamera;
+	Button btnContact;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		btnForm = (Button)findViewById(R.id.btnForm);
 		editText = (EditText)findViewById(R.id.editText);
 		textView = (TextView)findViewById(R.id.textView);
+		imageView = (ImageView)findViewById(R.id.imageView);
+		
+		btnForm = (Button)findViewById(R.id.btnForm);
+		btnCamera = (Button)findViewById(R.id.btnCamera);
 		
 		btnForm.setOnClickListener(new OnClickListener() {
 			
@@ -35,6 +46,15 @@ public class MainActivity extends Activity {
 				startActivityForResult(intent, FORMULARIO);
 			}
 		});
+		
+		btnCamera.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				startActivityForResult(intent, CAMARA);
+			}
+		});
 	}
 	
 	@Override
@@ -43,12 +63,17 @@ public class MainActivity extends Activity {
 		
 		switch (requestCode) {
 		case FORMULARIO:
-			if(resultCode == Activity.RESULT_OK){
+			if(resultCode == Activity.RESULT_OK) {
 				String formText = String.valueOf(data.getCharSequenceExtra("texto_formulario"));
 				textView.setText(formText);
 			}
 			break;
-
+		case CAMARA:
+			if(resultCode == Activity.RESULT_OK) {
+				Bundle extras = data.getExtras();
+		        Bitmap imageBitmap = (Bitmap) extras.get("data");
+				imageView.setImageBitmap(imageBitmap);
+			}
 		default:
 			break;
 		}
