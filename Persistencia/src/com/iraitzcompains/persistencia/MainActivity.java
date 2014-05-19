e666f9fa0d812fe06524876bce565deba9a1481d
Iraitz Compains <iracom82@gmail.com>
2014-05-19 11:22:38 +0200

    En el proyecto Persistencia, se ha modificado la forma de obtener la magnitud.

--- Persistencia/src/com/iraitzcompains/persistencia/MainActivity.java
+++ Persistencia/src/com/iraitzcompains/persistencia/MainActivity.java
@@ -107,7 +107,7 @@ public class MainActivity extends Activity {
 		
 		datoInterval.setText(strInterval);
 		
-		String strMagnitud = pf.getString("magnitud", "1");
+		String strMagnitud = pf.getString(getString(R.string.keyMagnitud), "1");
 		datoMagnitud.setText(strMagnitud);
 	}
 
