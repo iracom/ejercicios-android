4448dacbf6081380c7e42afcc3fee4631ecda051
Iraitz Compains <iracom82@gmail.com>
2014-05-30 13:20:09 +0200

    En el proyecto earthquake, se ha añadido un mapa al detalle y se muestra el lugar del terremoto en el mapa, de forma centrada.

--- Earthquake/src/com/iraitzcompains/earthquake/DetailActivity.java
+++ Earthquake/src/com/iraitzcompains/earthquake/DetailActivity.java
@@ -3,6 +3,14 @@ package com.iraitzcompains.earthquake;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -27,6 +35,8 @@ public class DetailActivity extends Activity implements LoaderCallbacks<Cursor>{
 	public final static int ID_EARTHQUAKE_LOADER = 2;
 	
 	TextView txtMag, txtPlace, txtTime;
+	MapFragment mapFragment;
+	GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,9 @@ public class DetailActivity extends Activity implements LoaderCallbacks<Cursor>{
 		txtPlace = (TextView)findViewById(R.id.txtDetailPlace);
 		txtTime = (TextView)findViewById(R.id.txtDetailTime);
 		
+		mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapDetalle);
+		
+		map = mapFragment.getMap();
 		
 		if (savedInstanceState == null) {
 			Intent intent = getIntent();
@@ -68,10 +81,25 @@ public class DetailActivity extends Activity implements LoaderCallbacks<Cursor>{
 			int indPlace = c.getColumnIndex(EarthquakeContentProvider.PLACE);
 			int indTime = c.getColumnIndex(EarthquakeContentProvider.TIME);
 			
+			int indLat = c.getColumnIndexOrThrow(EarthquakeContentProvider.LAT);
+			int indLon = c.getColumnIndexOrThrow(EarthquakeContentProvider.LONG);
+			
 			txtMag.setText(String.valueOf(c.getDouble(indMagnitude)));
 			txtPlace.setText(c.getString(indPlace));
 			SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss aaa", Locale.ENGLISH);
 			txtTime.setText(sdf.format(c.getLong(indTime)));
+			
+			if (map == null) {
+				map = mapFragment.getMap();
+			}
+			
+			double lat = c.getDouble(indLat);
+			double lon = c.getDouble(indLon);
+			MarkerOptions mOptions = new MarkerOptions().position(new LatLng(lat, lon)).title(c.getString(indPlace));
+			map.addMarker(mOptions);
+			
+			CameraUpdate center= CameraUpdateFactory.newLatLng(new LatLng(lat,lon));
+	        map.moveCamera(center);
 		}
 	}
 
